8fbf9b17aa8c8b56606e0564a0482ef1e2d74bf9
SSUHYUNKIM <yellosuhyun@naver.com>
2023-06-04 23:22:43 +0900

    #코드 처리하기

---
+++ LV0/코드 처리하기/solution.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string code) {
+    string answer = "";
+    int mode = 0;
+
+    for(int i = 0; i < code.size(); i++)
+    {
+        if(code[i] == '1')
+        {
+            if(mode == 0)
+                mode = 1;   
+            else
+                mode = 0;  
+        }
+        else if(mode == 0 && i % 2 == 0)
+            answer += code[i];
+        else if(mode == 1 && i % 2 == 1)
+            answer += code[i];
+    }
+    
+    if(answer.size() == 0)
+    {
+        answer = "EMPTY";
+    }
+    return answer;
+}

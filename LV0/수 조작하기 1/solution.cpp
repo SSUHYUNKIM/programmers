6a86e052b37ae50910117591cca2ff61b1aac17d
SSUHYUNKIM <yellosuhyun@naver.com>
2023-06-21 17:49:55 +0900

    #수 조작하기 1

---
+++ LV0/수 조작하기 1/solution.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, string control) {
+    int answer = 0;
+    for(int i = 0; i < control.size(); i++)
+    {
+        if(control[i] == 'w')
+            n++;
+        else if(control[i] == 's')
+            n--;
+        else if(control[i] == 'd')
+            n += 10;
+        else
+            n -= 10;
+    }
+    answer = n;
+    return answer;
+}
